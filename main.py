6481eeda212a4c162e60ba33ce37ea01a9231f9f
jfmarrero18 <jfmarrero18@gmail.com>
2025-06-17 16:46:40 -0400

    Fixed previous commit issue

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ def get_book_text(path):
     return file_contents
 
 def get_word_count(book):
-    number_of_words = 123
+    number_of_words = len(book.split())
+    return number_of_words
 
 def main():
     path = "/home/fabio/workspace/github.com/jfmarrero18/bookbot/bookbot/books/frankenstein.txt"
     book = get_book_text(path)
-    words =  book.split()
-    #print(book)
-    #book_chars = []
-    print(f"{len(words)} words found in the document")
-    text = "Hello there, world!"
+    words =  get_word_count(book)
+    print(f"{words} words found in the document")
 
 main()
